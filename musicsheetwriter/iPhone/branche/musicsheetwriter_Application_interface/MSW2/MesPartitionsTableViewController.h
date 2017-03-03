15ddda7e817c61a1a6d02a4ee8ac11164c767a7c
Ansou1 <simondaguenet81@gmail.com>
2017-03-03 12:22:56 +0100

    .

---
+++ musicsheetwriter/iPhone/branche/musicsheetwriter_Application_interface/MSW2/MesPartitionsTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  MesPartitionsTableViewController.h
+//  MSW
+//
+//  Created by simon on 15/12/2015.
+//  Copyright (c) 2015 Score Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MesPartitionsTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *pseudo;
+@property (strong, nonatomic) NSMutableArray *idRow;
+@property (strong, nonatomic) NSMutableArray *idRow2;
+@property (strong, nonatomic) NSMutableArray *suscribeRow;
+
+@end
---
+++ musicsheetwriter/iPhone/branche/musicsheetwriter_Application_interface/MSW2/PartitionSearchTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  PartitionSearchTableViewController.h
+//  MSW
+//
+//  Created by simon on 15/12/2015.
+//  Copyright (c) 2015 Score Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartitionSearchTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *pseudo;
+@property (strong, nonatomic) NSMutableArray *idRow;
+@property (strong, nonatomic) NSMutableArray *idRow2;
+@property (strong, nonatomic) NSMutableArray *suscribeRow;
+
+@end
